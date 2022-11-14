9d34b5b9c91e69b0ac95145c7370f75c2f8f2917
gimhanul <gimhanuly@gmail.com>
2022-11-14 14:35:48 +0900

    java project re init

---
+++ src/main/java/com/bamdoliro/bgcr/BgcrApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.bgcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamdoliro/bgcr/BgcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.bgcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
