e5a0f585f3c6909302a0bea7cf9581ab7f69fb3a
hsem4717 <hsem4717@gmail.com>
2022-11-17 19:16:41 +0900

    수정

--- src/main/java/com/bamdoliro/bgcr/domain/user/BgcrUser.java
+++ src/main/java/com/bamdoliro/bgcr/domain/user/BgcrUser.java
@@ -5,43 +5,42 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Data
 @Entity
 @NoArgsConstructor
-public class BgcrEntity {
+public class BgcrUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 15)
     private int studentNumber;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private int admissionYear;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private int rank;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private int amountOfCommit;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 300)
     private String profileImage;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String githubId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String githubBio;
 
     @Builder
-    public BgcrEntity(Long userId, String name, int studentNumber, int admissionYear, String email, int rank, int amountOfCommit, String profileImage, String githubId, String githubBio){
+    public BgcrUser(Long userId, String name, int studentNumber, int admissionYear, String email, int rank, int amountOfCommit, String profileImage, String githubId, String githubBio){
         this.userId = userId;
         this.name = name;
         this.studentNumber = studentNumber;
